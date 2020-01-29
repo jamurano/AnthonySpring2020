17f4e8d2c5b3c633ab3ba3d68dcb1780d1abd339
Unknown <jamurano@yahoo.com>
2020-01-28 23:47:07 -0700

    Practicing

--- New Unity Project/Assets/NumberWizard.cs
+++ New Unity Project/Assets/NumberWizard.cs
@@ -1,21 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int max = 1000;
+    private int min = 1;
+    
     void Start()
     {
+        
         Debug.Log("Welcome to number wizard");
         Debug.Log("Pick a number...");
-        Debug.Log("Highest number is: 1000");
-        Debug.Log("Lowest number is: 1");
+        Debug.Log("Highest number is: " + max);
+        Debug.Log("Lowest number is: " + min);
+        Debug.Log("Tell me your number is higher or lower than 500");
+        Debug.Log("Push up = higher, Push down = lower, Push enter = correct");
+        
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up arrow was pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down arrow was pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter button was pressed");
+        }
     }
 }
