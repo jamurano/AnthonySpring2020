d73ef1ed8eb4534e2fc0b2177916ca2190c5a981
Unknown <jamurano@yahoo.com>
2020-01-31 18:31:19 -0700

    Practice

--- New Unity Project/Assets/NumberWizard.cs
+++ New Unity Project/Assets/NumberWizard.cs
@@ -4,32 +4,36 @@ public class NumberWizard : MonoBehaviour
 {
     private int max = 1000;
     private int min = 1;
+    private int guess = 500;
     
     void Start()
     {
-        
         Debug.Log("Welcome to number wizard");
         Debug.Log("Pick a number...");
         Debug.Log("Highest number is: " + max);
         Debug.Log("Lowest number is: " + min);
-        Debug.Log("Tell me your number is higher or lower than 500");
+        Debug.Log("Tell me if your number is higher or lower than: " + guess);
         Debug.Log("Push up = higher, Push down = lower, Push enter = correct");
-        
+        max = max + 1;
     }
     
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up arrow was pressed");
+            min = guess;
+            guess = (max + min) / 2;
+            Debug.Log("Is your number higher or lower than " + guess + "?");
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down arrow was pressed");
+            max = guess;
+            guess = (max + min) / 2;
+            Debug.Log("Is your number higher or lower than " + guess + "?");
         }
-        if (Input.GetKeyDown(KeyCode.Return))
+        else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Enter button was pressed");
+            Debug.Log("I am a genius!");
         }
     }
-}
+} 
