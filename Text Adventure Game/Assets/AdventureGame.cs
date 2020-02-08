7676e999ba0773db9ef07c83ecd4749480c36830
Unknown <jamurano@yahoo.com>
2020-02-07 19:31:24 -0700

    Text Coding

---
+++ Text Adventure Game/Assets/AdventureGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+    void Start()
+    {
+        textComponent.text = ("Festers. Everywhere. Armed mostly with gas-powered rock flingers. " +
+                              "At least 10 of them guarding the hangar door. Typical." +
+                              "Could try punching my way through. Or sneaking the long way to the emergency exit. " +
+                              "What should I do?" +
+                              "1. Take them head on - rush the guards." +
+                              "2. Sneak my way to the emergency exit.");
+    }
+    
+    void Update()
+    {
+        
+    }
+}
