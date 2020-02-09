32064edfcbf763887275dd4d961641d7fb0257c4
Unknown <jamurano@yahoo.com>
2020-02-08 19:31:25 -0700

    Scriptable Objects State

--- Text Adventure Game/Assets/AdventureGame.cs
+++ Text Adventure Game/Assets/AdventureGame.cs
@@ -4,15 +4,13 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] private State startingState;
 
+    private State state;
     void Start()
     {
-        textComponent.text = ("Festers. Everywhere. Armed mostly with gas-powered rock flingers. " +
-                              "At least 10 of them guarding the hangar door. Typical." +
-                              "Could try punching my way through. Or sneaking the long way to the emergency exit. " +
-                              "What should I do?" +
-                              "1. Take them head on - rush the guards." +
-                              "2. Sneak my way to the emergency exit.");
+        state = startingState;
+        textComponent.text = state.GetStateStory();
     }
     
     void Update()
---
+++ Text Adventure Game/Assets/State.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)] [SerializeField] private string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+}
