e6fa43910af74802caaa36e050eb03f0667050dd
Unknown <jamurano@yahoo.com>
2020-02-11 17:56:24 -0700

    Health

---
+++ New Unity Project/Assets/Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Health : ScriptableObject
+{
+    public FloatData healthData;
+    
+    public FloatData[] damagerList;
+
+    public void ChangeHealth(FloatData incomingDamager)
+    {
+        foreach (var damager in damagerList)
+        {
+            if (damager == incomingDamager)
+            {
+                healthData.UpdateValue(damager.Value);
+            }
+        }
+    }
+
+}
