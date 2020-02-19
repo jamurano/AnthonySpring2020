b65b01513559f2d1603af7b39a2e36b76cb29aba
Unknown <jamurano@yahoo.com>
2020-02-18 19:30:47 -0700

    Player Movement Rotation and Player Attack

---
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{ 
+    public CharacterController controller;
+    
+    public float speed = 12f; 
+    public float gravity = -9.81f; 
+    public float jumpHeight = 3f;
+    
+    public Transform groundCheck; 
+    public float groundDistance = 0.4f;
+
+    private Vector3 velocity;
+    public bool isGrounded;
+    public bool isClimbing;
+    void Update() 
+    { 
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance);
+        float x = Input.GetAxis("Horizontal"); 
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(0,0,0); 
+        
+        if (isClimbing == false)
+        {
+            move = new Vector3(-x, 0, -z); 
+            if (x != 0 || z != 0) 
+            { 
+                transform.rotation = Quaternion.LookRotation(move); 
+            } 
+        }
+        
+        else 
+        { 
+            move = new Vector3(-x,z, 0); 
+        }
+        
+        controller.Move(move * speed * Time.deltaTime);
+        
+        if (Input.GetButtonDown("Jump") && isGrounded) 
+        { 
+            Debug.Log("jump button pressed"); 
+            velocity.y = jumpHeight; 
+        }
+        
+        if (isClimbing == false) 
+        { 
+            velocity.y += gravity * Time.deltaTime;
+            
+            controller.Move(velocity * Time.deltaTime); 
+        } 
+    }
+    public void OnTriggerEnter(Collider other) 
+    { 
+        if (other.tag == "Climbable") 
+        { 
+            isClimbing = true; 
+        } 
+    } 
+    public void OnTriggerExit(Collider other) 
+    { 
+        if (other.tag == "Climbable") 
+        { 
+            isClimbing = false; 
+        } 
+    } 
+}
