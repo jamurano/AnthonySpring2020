d1d3ead4e9f50ecb17ec9fe565e2dfac8f7955d4
Unknown <jamurano@yahoo.com>
2020-02-20 19:56:54 -0700

    HitBox

---
+++ New Unity Project/Assets/HitBox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    public int damage;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<HealthData>())
+        {
+            other.GetComponent<HealthData>().TakeDamage(damage);
+        }
+    }
+}
