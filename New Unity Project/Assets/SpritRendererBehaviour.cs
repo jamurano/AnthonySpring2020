95c6c06084373b0ca66cd8af77c9f8c717b2e243
Unknown <jamurano@yahoo.com>
2020-02-25 13:10:03 -0700

    InClass ColorData

---
+++ New Unity Project/Assets/SpritRendererBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpritRendererBehaviour : MonoBehaviour
+{
+    private SpriteRenderer spriteRendererObj;
+
+    public ColorData ColorDataObj;
+
+    public UnityEvent startEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRendererObj = GetComponent<SpriteRenderer>();
+        startEvent.Invoke();
+    }
+
+    public void ChangeSpriteColor(ColorData colorDataObj)
+    {
+        spriteRendererObj.color = colorDataObj.value;
+    }
+}
+
