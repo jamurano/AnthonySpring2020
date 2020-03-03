312aebe4ae91090760105394870b63678783c042
Unknown <jamurano@yahoo.com>
2020-03-03 15:54:39 -0700

    InClass

---
+++ New Unity Project/Assets/UI/TextBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class TextBehaviour : MonoBehaviour
+{
+    
+    private Text textObj;
+    public StringListData stringListDataObj;
+    void Awake()
+    {
+        textObj = GetComponent<Text>();
+    }
+    
+    void Update()
+    {
+        textObj.text = stringListDataObj.ReturnCurrentLine();
+    }
+}
