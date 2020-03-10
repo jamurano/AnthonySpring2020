0d7e73936ad5f6d815d571565f009335bb580a98
Unknown <jamurano@yahoo.com>
2020-03-10 16:52:16 -0600

    Collectables, Collections

---
+++ New Unity Project/Assets/Scripts/Collections.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Collections : ScriptableObject
+{
+    public List<Collectable> collectablesList;
+
+    public void AddToCollection(Collectable collectableObj)
+    {
+        collectablesList.Add(collectableObj);
+    }
+
+    public void RemoveFromCollection(Collectable collectableObj)
+    {
+        for (var index = collectablesList.Count - 1; index >= 0; index--)
+        {
+            var obj = collectablesList[index];
+            if (obj == collectableObj)
+            {
+                collectablesList.Remove(collectableObj);
+            }
+        }
+    }
+
+    public void ClearCollection()
+    {
+        collectablesList.Clear();
+    }
+}
