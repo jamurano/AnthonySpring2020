3f120fd857690dc27e1a56273ee9b4d68f3258a5
Unknown <jamurano@yahoo.com>
2020-03-09 21:20:32 -0600

    InventoryUI, PauseMenu

---
+++ New Unity Project/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject healthImage, manaImage, coinImage;
+
+    public PickUp.pickUpType pickUpType;
+
+    public void InventoryManager(GameObject pickUp)
+    {
+        //find out from PickUp what object it is
+        pickUpType = pickUp.GetComponent<PickUp>().pickUps;
+        //turn on correlating image and value into InventorySlot
+        if (pickUpType == PickUp.pickUpType.MANA)
+        {
+            manaImage.SetActive(true);
+        }
+
+        if (pickUpType == PickUp.pickUpType.COINS)
+        {
+            coinImage.SetActive(true);
+        }
+
+        if (pickUpType == PickUp.pickUpType.HEALTH)
+        {
+            healthImage.SetActive(true);
+        }
+    }
+}
