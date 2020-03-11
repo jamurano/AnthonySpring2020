0fdbf1fd0524b524c0ed91132c64846c0f18362a
Unknown <jamurano@yahoo.com>
2020-03-10 19:19:43 -0600

    InventoryMenu

---
+++ New Unity Project/Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour
+{
+    public static bool InventoryIsUp = false;
+
+    public GameObject inventoryMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (InventoryIsUp)
+            {
+                CloseInventory();
+            }
+            else
+            {
+                OpenInventory();
+            }
+        }
+    }
+
+    void CloseInventory()
+    {
+        inventoryMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        InventoryIsUp = false;
+        PauseMenu.GameIsPaused = false;
+    }
+    
+    void OpenInventory()
+    {
+        inventoryMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        InventoryIsUp = true;
+        PauseMenu.GameIsPaused = true;
+    }
+
+}
